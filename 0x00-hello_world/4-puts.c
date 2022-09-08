f039ca1d329b0aba39d88336f1482e5d4fd64eed
Adelahpaul <80081468+Adelahpaul@users.noreply.github.com>
2022-09-08 22:59:23 +0100

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 
 /**
 
- * main - prints the phrase "Programming is like building a multilingual puzzle
-
+ * main - prints the phrase 
+ *
  * Return: 0 if exited properly, non-zero otherwise
 
  */
